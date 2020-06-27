02b626f7e51bb8f9051b25ab6ec8876441a81291
Crowdbotics <team@crowdbotics.com>
2020-06-27 21:24:14 +0000

    Update models

--- backend/planet_funny_18454/wsgi.py
+++ backend/planet_funny_18454/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'planet_funny_18454.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "planet_funny_18454.settings")
 
 application = get_wsgi_application()
